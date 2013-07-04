37b30158666ae33e05a833058f564499aea397af
Harshit Upadhyay <hjupadhyay@gmail.com>
2013-07-04 12:20:47 +0000

    web.js now reading index from buffer

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var buffer=require('buffer').buffer;
+var buffer = fs.readFileSync('index.html');
+var display = buffer.toString("utf-8");
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(display);
 });
 
 var port = process.env.PORT || 5000;
